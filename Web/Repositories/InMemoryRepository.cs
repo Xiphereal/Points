7b88efa98e00bad902db5074fe7be85cc10f3b1e
Xiphereal <lozanojalex@gmail.com>
2025-08-30 00:37:29 +0200

    refactor: encapsulate in repository the points and ideal points

---
+++ Web/Repositories/InMemoryRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Web.Repositories;
+
+public class InMemoryRepository
+{
+    public int Points { get; private set; }
+    public int IdealPoints { get; private set; }
+
+    public void ContributeWith(int points)
+    {
+        Points += points;
+        IdealPoints += points;
+    }
+
+    public void AddIdealPoints(int howMany)
+    {
+        IdealPoints += howMany;
+    }
+
+    public void ResetPoints()
+    {
+        Points = IdealPoints = 0;
+    }
+}
