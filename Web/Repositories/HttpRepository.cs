0e92c8835490369142b2ddd7f5f6faafefacb8ab
Xiphereal <lozanojalex@gmail.com>
2025-09-15 19:08:47 +0200

    Revert "perf: read through cache"
    This reverts commit df1cf0b8bb23141bdfc58dedda578ced3d690ba3.

--- Web/Repositories/HttpRepository.cs
+++ Web/Repositories/HttpRepository.cs
@@ -6,22 +6,17 @@ namespace Web.Repositories;
 public class HttpRepository(string baseUrl)
 {
     private readonly HttpClient httpClient = new();
-    private PointsBalanceDto? cachedPointsBalance;
 
     public async Task<PointsBalanceDto> GetPointsBalance()
     {
         var response = await httpClient.GetAsync($"{baseUrl}/GetPointsBalance");
 
-        var pointsBalance = 
-            cachedPointsBalance =
-                await response.Content.ReadAsAsync<PointsBalanceDto>();
-
-        return pointsBalance;
+        return await response.Content.ReadAsAsync<PointsBalanceDto>();
     }
 
     public async Task ContributeWith(int points)
     {
-        var previousPointsBalance = await GetPointsBalanceWithReadThroughCache();
+        var previousPointsBalance = await GetPointsBalance();
 
         var adjustedPointsBalance = new PointsBalanceDto(
             Points: previousPointsBalance.Points + points,
@@ -32,17 +27,9 @@ public class HttpRepository(string baseUrl)
             adjustedPointsBalance);
     }
 
-    private async Task<PointsBalanceDto> GetPointsBalanceWithReadThroughCache()
-    {
-        if (cachedPointsBalance is not null)
-            return cachedPointsBalance;
-
-        return await GetPointsBalance();
-    }
-
     public async Task AddIdealPoints(int howMany)
     {
-        var previousPointsBalance = await GetPointsBalanceWithReadThroughCache();
+        var previousPointsBalance = await GetPointsBalance();
 
         var adjustedPointsBalance = new PointsBalanceDto(
             Points: previousPointsBalance.Points,
