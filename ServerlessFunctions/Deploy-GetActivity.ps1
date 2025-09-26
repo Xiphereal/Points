2ed9afd8cb4e2b898340be8efaf0694b5b3147bb
Xiphereal <lozanojalex@gmail.com>
2025-09-26 23:57:11 +0200

    feat: new Lambda Function for getting the Activity

---
+++ ServerlessFunctions/Deploy-GetActivity.ps1
@@ -0,0 +1,4 @@
+﻿dotnet lambda deploy-function GetActivity `
+    --function-handler "ServerlessFunctions::ServerlessFunctions.GetActivity::FunctionHandler"
+
+Write-Output "Published"
---
+++ ServerlessFunctions/GetActivity.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Nodes;
+using Amazon.Lambda.Core;
+using DTOs;
+using Npgsql;
+using static ServerlessFunctions.Utils;
+
+namespace ServerlessFunctions;
+
+public class GetActivity
+{
+    public async Task<ActivityDto?> FunctionHandler(
+        JsonNode input,
+        ILambdaContext context)
+    {
+        NpgsqlConnection? connection = null;
+        
+        try
+        {
+            connection = await OpenConnectionToRds();
+
+            return await GetActivityFromDatabase(connection);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+        finally
+        {
+            await connection?.CloseAsync()!;
+        }
+
+        return null;
+    }
+
+    private async Task<ActivityDto?> GetActivityFromDatabase(NpgsqlConnection connection)
+    {
+        const string sql = @"SELECT * FROM ""Activity""";
+        await using var command = new NpgsqlCommand(sql, connection);
+
+        await using var reader = await command.ExecuteReaderAsync();
+
+        List<string> activity = [];
+        while (await reader.ReadAsync())
+            activity.Add(reader.GetString(1));
+        
+        return new ActivityDto(activity);
+    }
+}
